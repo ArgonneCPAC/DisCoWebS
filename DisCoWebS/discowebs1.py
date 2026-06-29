46e025e8d9188f15b0d88f6038c0986c05a3f7fe
MitraKaustav <k.mitra.kaustav@gmail.com>
2026-06-29 11:32:14 -0500

    added discowebs1 for 1-data fit.

---
+++ DisCoWebS/discowebs1.py
@@ -0,0 +1,76 @@
+from DisCoWebS.data_loader import clean_cosmos20_loader as cc20
+from DisCoWebS.data_loader import sdss_loader as sdl
+from DisCoWebS.data_loader import cosmos_web_loader as cwl
+from DisCoWebS.data_loader import load_ssp_templates
+from DisCoWebS.data_loader import sdss_filter_select
+from DisCoWebS.data_loader import cosmos_web_filter_select
+from DisCoWebS.optimizer import opt1_4d
+from DisCoWebS.data_loader.param_collection import (
+    load_param_collection,
+    write_param_collection,
+)
+from DisCoWebS import config
+from DisCoWebS.modelling.igm import load_igm_attenuation_table
+
+mock_version_name = "discowebs_cosmosweb"
+config.mock_version_name = mock_version_name
+
+if __name__ == "__main__":
+    # Load data:
+    cosmos_web = cwl.load_cosmos_web_without_MIRI(
+        app_mag_f444w_cut=27.0, z_min=0.3, z_max=3.0
+    )
+
+    # Choose filters. Load transmission curves and SSP templates.
+    cosmos_web_filters_to_use = [
+        "chft_u",
+        "hsc_g",
+        "hsc_r",
+        "hsc_i",
+        "hsc_z",
+        "hsc_y",
+        "nircam_f115w",
+        "nircam_f150w",
+        "nircam_f277w",
+        "nircam_f444w",
+    ]
+    tcurves_cosmos_web, ssp_data_cosmos_web, cosmos_web_mag_colnames = (
+        cosmos_web_filter_select.cosmos_web_filters(cosmos_web_filters_to_use)
+    )
+    config.cosmos_web_filters_to_use = cosmos_web_filters_to_use
+
+    # Load IGM attenuation interpolation table:
+    config.igm_attenuation_table = load_igm_attenuation_table()
+
+    # Choose a set of initial parameters:
+    init_params_bounded = load_param_collection(
+        drn_mock="~/calibrated_params/", mock_version_name="discowebs1"
+    )
+
+    # Run optimization:
+    params_bf_bounded, best_loss = opt1_4d.run_optimization_1data(
+        data=cosmos_web,
+        data_mag_colnames=cosmos_web_mag_colnames,
+        N_z_bins=10,
+        N_host_min=1000,
+        N_host_max=1000,
+        n_z_phot_table=10,
+        ssp_data=ssp_data_cosmos_web,
+        tcurves=tcurves_cosmos_web,
+        N_mag_bins=10,
+        N_color_bins=10,
+        ndsig_by_dbin=0.5,
+        num_loops=10,
+        num_steps_per_loop=100,
+        modes=["fix_diffstarpop_merging", "fix_diffstarpop", "fit_all"],
+        data_to_use="cosmos_web",
+        loss_type="log_mse_loss",
+        init_params_bounded=None,
+    )
+
+    # Save the best-fit parameters:
+    write_param_collection(
+        drn_mock="~/calibrated_params",
+        mock_version_name=config.mock_version_name,
+        param_collection=params_bf_bounded,
+    )
